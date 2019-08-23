693e2033a389561843b9d921d713aee3a1b5388e
Vimerum <veronese.goncalves@aluno.ufabc.edu.br>
2019-08-22 23:07:47 -0300

    Add header and footer

--- NoLanguageCompiler/NoLanguageCompiler.java
+++ NoLanguageCompiler/NoLanguageCompiler.java
@@ -1,4 +1,4 @@
-import java.io.FileReader;
+import java.io.*;
 
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
@@ -14,8 +14,17 @@ public class NoLanguageCompiler {
         } else {
             String fileName = args[0];
             try {
-                FileReader file = new FileReader(fileName);
-                compile(file);
+                // Printa o Header
+                printHeader();
+
+                System.out.printf("Iniciando compilacao do arquivo: %s\n", fileName);
+                // Cria um Stream que le o arquivo
+                CharStream input = CharStreams.fromFileName(fileName);
+                compile(input);
+
+                System.out.println("Arquivo compilado com SUCESSO.\n");
+                // Printa o Footer
+                printFooter();
             } catch (Exception e) {
                 System.out.println("[ERRO 003]: Ocorreu uma excecao:");
                 System.out.println(e.getMessage());
@@ -27,9 +36,7 @@ public class NoLanguageCompiler {
      * O codigo abaixo foi retirado do livro "The Definitive ANTLR4 Reference" de Terence Parr.
      * Parr, T. (2013). The Definitive ANTLR4 Reference. 1st ed. Dallas, Texas: The Pragmatic Bookshelf, p.29.
      **/
-    public static void compile (FileReader file) throws Exception {
-        // Cria um Stream que le o arquivo
-        ANTLRInputStream input = new ANTLRInputStream(file);
+    public static void compile (CharStream input) throws Exception {
 
         // Cria um lexer a partir do arquivo
         NoLanguageLexer lexer = new NoLanguageLexer(input);
@@ -46,6 +53,29 @@ public class NoLanguageCompiler {
         
         // Anda pela arvore criada pela traducao, chamando os callbacks
         walker.walk(new NoLanguageTranslateListener(), tree);
-        System.out.println("Acabou");
+    }
+
+    private static void printHeader () throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader("NoLanguageHeader.txt"));
+
+        String line = reader.readLine();
+
+        while(line != null) {
+            System.out.println(line);
+            line = reader.readLine();
+        }
+
+        System.out.println();
+    }
+
+    private static void printFooter () throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader("NoLanguageFooter.txt"));
+
+        String line = reader.readLine();
+
+        while(line != null) {
+            System.out.println(line);
+            line = reader.readLine();
+        }
     }
 }
